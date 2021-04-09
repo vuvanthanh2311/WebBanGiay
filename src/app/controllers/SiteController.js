44aaf8607a912fd5476871e4e0091617ceba4ba8
thongdepzai <18521458@gm.uit.edu.vn>
2021-04-09 15:51:46 +0700

    read data in mongodb

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -25,5 +25,53 @@ class SiteController {
     search(req, res) {
         res.render('search');
     }
+
+    sport(req, res, next) {
+        //  res.render('men/sport');
+        Product.find({ type:"sport"})
+            .then((product) => {
+                res.render('men/sport', {
+                    product: mutipleMongooseObject(product),
+                });
+            })
+            .catch(next);
+    }
+
+    lazy(req, res, next) {
+        //  res.render('men/lazy');
+        Product.find({type:"lazy"})
+            .then((product) => {
+                res.render('men/lazy', {
+                    product: mutipleMongooseObject(product),
+                });
+            })
+            .catch(next);
+    }
+    
+    leather(req, res, next) {
+        //  res.render('men/leather');
+        Product.find({type:"leather"})
+            .then((product) => {
+                res.render('men/leather', {
+                    product: mutipleMongooseObject(product),
+                });
+            })
+            .catch(next);
+    }
+    
+    sandan(req, res, next) {
+        //  res.render('men/sandan');
+        Product.find({type:"sandan"})
+            .then((product) => {
+                res.render('men/sandan', {
+                    product: mutipleMongooseObject(product),
+                });
+            })
+            .catch(next);
+    }
+    
+
+
+    
 }
 module.exports = new SiteController();
--- src/routes/sites.js
+++ src/routes/sites.js
@@ -6,5 +6,9 @@ router.get('/news/:slug', siteController.newsshow);
 router.get('/news', siteController.news);
 router.get('/search', siteController.search);
 router.get('/', siteController.home);
+router.get('/men/sport', siteController.sport);
+router.get('/men/lazy', siteController.lazy);
+router.get('/men/leather', siteController.leather);
+router.get('/men/sandan', siteController.sandan);
 
 module.exports = router;
